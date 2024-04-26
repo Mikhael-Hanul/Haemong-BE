aeb030cb0047a692ac92bfb2d0bd1c461600559d
yoochanhong <chanhongy6@naver.com>
2024-04-26 09:47:38 +0900

    mysql 연결

--- haemong-be/main.go
+++ haemong-be/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"haemong-be/controller"
 	"haemong-be/repository"
 	"haemong-be/service"
+	"os"
 )
 
 func main() {
 	app := fiber.New()
+	password := os.Getenv("PASSWORD")
+	db, err := sql.Open("mysql", "root:"+password+"@tcp(localhost:3306)/haemong")
+	if err != nil {
+		fmt.Println("db boom..! ", err)
+		return
+	}
 
-	userRepository := repository.NewUserRepository()
-	userService := service.NewUserService(*userRepository)
-	userController := controller.NewUserController(*userService)
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(userRepository)
+	userController := controller.NewUserController(userService)
 
 	user := app.Group("/user")
 	user.Post("/sign-up", userController.UserController)
 
+	_ = app.Listen(":8080")
 }
