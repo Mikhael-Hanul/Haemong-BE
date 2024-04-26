9da302d07ee882848d94dc31f4e53932443ef1a1
yoochanhong <chanhongy6@naver.com>
2024-04-26 09:47:29 +0900

    회원가입 기능 구현

--- haemong-be/repository/user_repository.go
+++ haemong-be/repository/user_repository.go
@@ -1,16 +1,36 @@
 package repository
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type UserRepository struct {
+	db *sql.DB
 }
 
-func NewUserRepository() *UserRepository {
-	return &UserRepository{}
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
 }
 
-func (r *UserRepository) SaveUser(userId, userName, password string) error {
+type UserEntity struct {
+	userId   string
+	password string
+	name     string
+}
+
+func (r *UserRepository) SaveUser(userId, password, name string) error {
+	_, err := r.db.Exec("insert into tbl_user value (?, ?, ?)", userId, password, name)
+	if err != nil {
+		return fmt.Errorf("유저 등록에 실패함 : " + err.Error())
+	}
 	return nil
 }
 
 func (r *UserRepository) IsUserIdDuplicate(userId string) bool {
-	return false
+	var u UserEntity
+	err := r.db.QueryRow("select * from tbl_user where userId = ?", userId).Scan(&u.userId, &u.password, &u.name)
+	return err == nil
 }
--- haemong-be/service/user_service.go
+++ haemong-be/service/user_service.go
@@ -1,24 +1,24 @@
 package service
 
 import (
-	"fmt"
+	"errors"
 	"haemong-be/repository"
 )
 
 type UserService struct {
-	repo repository.UserRepository
+	repo *repository.UserRepository
 }
 
-func NewUserService(repo repository.UserRepository) *UserService {
+func NewUserService(repo *repository.UserRepository) *UserService {
 	return &UserService{
 		repo: repo,
 	}
 }
 
-func (r *UserService) SignUp(userName, userId, password string) error {
+func (r *UserService) SignUp(name, userId, password string) error {
 	isUserIdDuplicate := r.repo.IsUserIdDuplicate(userId)
 	if isUserIdDuplicate {
-		return fmt.Errorf("아이디 중복")
+		return errors.New("아이디 중복")
 	}
-	return r.repo.SaveUser(userId, userName, password)
+	return r.repo.SaveUser(userId, password, name)
 }
